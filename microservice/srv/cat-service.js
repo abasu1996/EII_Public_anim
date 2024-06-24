5bb7698bbb0d2f21b39d2fe08c429be22f41d074
abasu1996 <animbasu1996@outlook.com>
2024-06-24 17:01:59 +0000

    New changes

--- microservice/srv/cat-service.js
+++ microservice/srv/cat-service.js
@@ -1,12 +1,15 @@
 
 module.exports = (say) => {
   say.on("upload", (req, res) => {
+    const cust_pos_resp = {
+      "noChanges": true
+    }
     let data = req._.req.body;
     const jsondata = JSON.stringify(data);
     const jsonstring = JSON.parse(jsondata);
     const ID = jsonstring.currentImage.id;
     const casetype = jsonstring.currentImage.caseType;
-    const status = jsonstring.currentImage.status;
+    //const status = jsonstring.currentImage.status;
     data = data.currentImage.id;
     console.log(ID);
     const response = {
@@ -20,54 +23,72 @@ module.exports = (say) => {
       ]
     }
 
-    const positiveresp = {
-      "noChanges": true
-    }
     const jsonresp = JSON.stringify(response);
     const jsonrespj = JSON.parse(jsonresp);
     if (casetype === "ZCMP") {
       req._.res.send(jsonrespj);
     }
     else {
-      req._.res.send(positiveresp);
+      req._.res.send(cust_pos_resp);
     }
 
     //return response;
   });
+
+
+
+
+
+
+
+
+
+  /****************CUSTOMER LOGIC ***************/
+
   say.on("customer", (req, res) => {
+    const cust_pos_resp = {
+      "noChanges": true
+    }
     let data = req._.req.body;
     const jsondata = JSON.stringify(data);
     const jsonstring = JSON.parse(jsondata);
     let accountTeam = jsonstring.currentImage.accountTeamMembers;
     let accountcreationscreen = jsonstring.beforeImage;
     let accountTeamrole = "";
-    let readyforsample_ex = jsonstring.extensions.Z_ReadyForSample;
+    let readyforsample_ex = jsonstring.currentImage.extensions.Z_ReadyForSample;
     if (accountcreationscreen !== null) {
       accountTeam.forEach(element => {
         if (element.role === "ZCR") {
           accountTeamrole = element.role;
         }
       });
-      if (accountTeamrole === "ZCR" && readyforsample_ex === false) {
-        const cust_pos_resp = {
-          "noChanges": true
+      if(readyforsample_ex)
+      {
+        if (accountTeamrole === "ZCR") {
+  
+          req._.res.send(cust_pos_resp);
         }
-        req._.res.send(cust_pos_resp);
+        else{
+          const cust_neg_resp = {
+            "noChanges": true,
+            "error": [
+              {
+                "code": "external_AccountService.10000",
+                "message": "Sorry! ZCR Role in not maintained in Account Team, Please maintain and proceed",
+                "target": "{accountTeamMembers.role}"
+              }
+            ]
+          };
+          req._.res.send(cust_neg_resp);
+        }
       }
-      else if (accountTeamrole !== "ZCR" && readyforsample_ex === true) {
-        const cust_neg_resp = {
-          "noChanges": true,
-          "error": [
-            {
-              "code": "external_AccountService.10000",
-              "message": "Sorry! ZCR Role in not maintained in Account Team, Please maintain and proceed",
-              "target": "{accountTeamMembers.role}"
-            }
-          ]
-        };
-        req._.res.send(cust_neg_resp);
-      }
-      req._res.send({
+      else
+      req._.res.send(cust_pos_resp);
+
+    }
+    else
+    {
+      const noval_creationScreen = {
         "noChanges": true,
         "info": [
           {
@@ -78,11 +99,21 @@ module.exports = (say) => {
           }
         ]
 
-      });
+      }
+      req._.res.send(noval_creationScreen);
     }
     //req._.res.send(jsonstring);
 
   });
+
+
+
+
+
+
+
+
+
 
   say.on("lead", (req, res) => {
 
@@ -94,6 +125,21 @@ module.exports = (say) => {
 
   });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   say.on("opportunity", (req, res) => {
 
     let data = req._.req.body;
@@ -103,6 +149,19 @@ module.exports = (say) => {
     req._.res.send(jsonstring);
 
   });
+
+
+
+
+
+
+
+
+
+
+
+
+  
   say.on("quote", (req, res) => {
 
     let data = req._.req.body;
