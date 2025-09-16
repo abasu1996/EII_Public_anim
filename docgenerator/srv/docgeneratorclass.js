5840e733a74a9d8489bf37a42cd9da57e8b9c78a
Basu, Anim <anim.basu@kps.com>
2025-09-16 16:25:47 +0530

    new docgenerator file added

---
+++ docgenerator/srv/docgeneratorclass.js
@@ -0,0 +1,16 @@
+import { saveAs } from "file-saver";
+import {
+    Alignment,
+    Paragraph,
+    TextRun,
+    AlignmentType,
+    Document,
+    Packer,
+    HeadingLevel,
+    TabStop,
+    TabStopPosition,
+    TabStopType
+} from "docx";
+
+
+
